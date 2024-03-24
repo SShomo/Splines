584d2e26f3c0907cc239dc51cb88264a177880b9
S_Shomo <90357187+SShomo@users.noreply.github.com>
2024-03-24 14:59:26 -0400

    Finished the spline part

--- spleens/Assets/Scripts/CreatePoints.cs
+++ spleens/Assets/Scripts/CreatePoints.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CreatePoints : MonoBehaviour
@@ -29,44 +30,42 @@ public class CreatePoints : MonoBehaviour
     private void Update()
     {
         pointArr.Clear();
-        for (int i = 0; i < points.Count; i++)
+        
+        int i = 0;
+        while (i < points.Count - 1)
         {
-            if(i != points.Count - 1)
-                CreateSpline(steps, points[i], points[i + 1]);
-            else
-                CreateSpline(steps, points[i], points[0]);
+            CreateSpline(steps, points[i], points[i + 1]);
+            i++;
         }
-    }
-
-    //Polynomial Coefficient Curve
-    private Vector3 GetCurve(Vector3 s1, Vector3 s2, Vector3 s3, Vector3 s4)
-    {
-        Vector3 a = Vector3.Lerp(s1, s2, t);
-        Vector3 b = Vector3.Lerp(s2, s3, t);
-        Vector3 c = Vector3.Lerp(s3, s4, t);
-        Vector3 d = Vector3.Lerp(a, b, t);
-        Vector3 e = Vector3.Lerp(b, c, t);
-        Vector3 curve = Vector3.Lerp(d, e, t);
-        //float sqd = Mathf.Pow(t, 2);
-        //float cud = Mathf.Pow(t, 3);
-        //Vector3 curve = s1 + t*(-3*s1 + 3*s2)+ sqd*(3*s1 - 6*s2 + 3*s3) + cud*(-s1 + 3*s2 - 3*s3 + s4);
-        return curve;
+        if(points.Count > 2)
+         CreateSpline(steps, points[i], points[0]);
     }
 
     private void CreateSpline(int steps, GameObject s1, GameObject s2)
     {
         GameObject so1 = s1.gameObject.GetComponent<SPoint>().otherPoint;
         GameObject so2 = s2.gameObject.GetComponent<SPoint>().otherPoint;
-        //t = Mathf.Clamp01(t);
-        for (int i = 1; i < steps + 1; i++)
+        float i = 0.0f;
+        while (i <= 1.1f)
         {
-            if (i != 0)
-                t = 1.0f / i;
-            else
-                t = 0.0f;
+            t = i;
             pointArr.Add(GetCurve(s1.transform.position, so1.transform.position, so2.transform.position, s2.transform.position));
+            i += (float)(1.0/steps);
         }
         lineRenderer.positionCount = pointArr.Count;
         lineRenderer.SetPositions(pointArr.ToArray());
+    }
+
+    //Polynomial Coefficient Curve
+    private Vector3 GetCurve(Vector3 s0, Vector3 s1, Vector3 s2, Vector3 s3)
+    {
+
+        Vector3 a = Vector3.Lerp(s0, s1, t);
+        Vector3 b = Vector3.Lerp(s1, s2, t);
+        Vector3 c = Vector3.Lerp(s2, s3, t);
+
+        Vector3 d = Vector3.Lerp(a, b, t);
+        Vector3 e = Vector3.Lerp(b, c, t);
+        return Vector3.Lerp(d, e, t);
     }
 }
